b74b53af01ca04fc813edaeabf90261734f660d7
edwardzuniga <4593@holbertonschool.com>
2022-05-03 21:52:08 -0500

    no q no e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for alfabeto in range(97, 123):
-    if alfabeto == 101 or alfabeto== 113:
+    if alfabeto == 101 or alfabeto == 113:
         continue
     print('{:c}'.format(alfabeto), end="")
