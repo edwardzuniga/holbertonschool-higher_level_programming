39aa4a42f77f8eeac94538e7a85dffa1e55c3118
edwardzuniga <4593@holbertonschool.com>
2022-05-05 13:14:29 -0500

    printarguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,14 @@
 if __name__ == "__main__":
     from sys import argv
 
-if len(argv) == 1:
-    print("0 arguments.")
-elif len (argv) == 2:
-    print("1 argument:")
-    print("1 {:s}".format(argv[1]))
+    if len(argv) == 1:
+        print("0 arguments.")
+    if len (argv) == 2:
+        print("1 argument:")
+        print("1 {:s}}".format(argv[1]))
 
-elif len(argv) > 2:
+    elif len(argv) > 2:
 
-    print("{:d} arguemnts:".format(len(argv) -1))
-    for i in range(1, len(argv)):
+        print("{:d} arguemnts:".format(len(argv) -1))
+    for i in range (1, len(argv)):
         print("{:d}: {:s}".format(i, argv[i]))
