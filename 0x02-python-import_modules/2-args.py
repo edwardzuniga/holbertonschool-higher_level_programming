1aeccad53321fa8f755a8b490959a8ec8b8b02d6
edwardzuniga <4593@holbertonschool.com>
2022-05-05 13:08:19 -0500

    printarguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,5 +11,5 @@ elif len (argv) == 2:
 elif len(argv) > 2:
 
     print("{:d} arguemnts:".format(len(argv) -1))
-    for i in range (1, len(argv)):
+    for i in range(1, len(argv)):
         print("{:d}: {:s}".format(i, argv[i]))
