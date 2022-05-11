2d89ec3e8967cb57155acfc3a45be9661d67b9de
edwardzuniga <4593@holbertonschool.com>
2022-05-11 13:47:37 -0500

    removechar

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     remov = ""
     for char in my_string:
-        if char != 'c' and char != 'C':
+        if char != "c" and char != "C":
             remov += char
-            return remov
+    return remov
