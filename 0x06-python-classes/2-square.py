a2abf7a8d2505d99feed7f1db543e18abbd614a0
edwardzuniga <4593@holbertonschool.com>
2022-05-25 17:56:38 -0500

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,8 +3,8 @@
 
 
 class Square:
-    """A class Square that defines a square by: 
-    based on 1-square.py"""
+    """A class Square that defines a 
+    square by: based on 1-square.py"""
 
     def __init__(self, size=0):
         """Start"""
