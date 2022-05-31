7c92e2fcc5a4333675544bf3261046fb9f420411
edwardzuniga <4593@holbertonschool.com>
2022-05-30 22:13:40 -0500

    2. Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Say my name"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    function that prints My name is <first name> <last name>
+    """
+    if isinstance (first_name, str) is False:
+        raise TypeError("first_name must be a string")
+    if isinstance (last_name, str) is False:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
