b729dc26c6018765196c1cb211300eb9098ddf56
edwardzuniga <4593@holbertonschool.com>
2022-05-30 22:11:55 -0500

    0. Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Integer addition"""
+
+
+def add_integer(a, b=98):
+    """
+    Function adds two integers
+    """
+    if isinstance(a, (int, float)) is False:
+        raise TypeError('a must be an integer')
+    if isinstance(b, (int, float)) is False:
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
