f2b826d22c4e399e027546659336b6730d77372e
edwardzuniga <4593@holbertonschool.com>
2022-05-31 09:29:56 -0500

    3. Print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
+
 """Print square"""
+
 
 def print_square(size):
     """
     prints a square with the character #
     """
-    if isinstance (size, int) is False:
+    if isinstance(size, int) is False:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
