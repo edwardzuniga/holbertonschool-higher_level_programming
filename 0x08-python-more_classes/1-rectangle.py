3d352c132ba1688646d2b24a98f65e812509bd37
edwardzuniga <4593@holbertonschool.com>
2022-06-02 15:37:13 -0500

    1rect

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,7 @@
 class Rectangle:
     """Rectangle class"""
 
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
