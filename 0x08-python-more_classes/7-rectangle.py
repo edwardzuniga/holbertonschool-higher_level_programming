2b81d63a432b935c8464adaadb40d62ec0e4c5c6
edwardzuniga <4593@holbertonschool.com>
2022-06-02 19:23:11 -0500

    Change representation

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+""" Module for Rectangle class. """
+
+
+class Rectangle:
+    """
+    Representation of a Rectangle.
+    """
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """ initialization """
+        self.width = width
+        self.height = height
+        self.__class__.number_of_instances += 1
+
+    def __str__(self):
+        if not self.__width or not self.__height:
+            return ""
+        w = self.width
+        h = self.height
+        return ((str(self.print_symbol) * w + '\n') * h)[:-1]
+
+    def __repr__(self):
+        return "Rectangle(" + str(self.width) + ", " + str(self.height) + ")"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        self.__class__.number_of_instances -= 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if not self.__width or not self.__height:
+            return 0
+        return (self.__width + self.__height) * 2
