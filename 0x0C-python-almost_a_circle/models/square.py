8a6a4b08f30ac4296a80aa025f5e2ca05c56f334
edwardzuniga <4593@holbertonschool.com>
2022-06-14 17:04:50 -0500

    square1

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""And now, the Square!"""
+
+
+from ctypes import sizeof
+from logging.config import valid_ident
+from models.rectangle import Rectangle
+
+
+class Square (Rectangle):
+    """Inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        '''return str'''
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.x, self.y, self.width)
