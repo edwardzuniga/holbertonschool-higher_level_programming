a649ac2ea424c754f314fa31b310bc741ec6e1dd
edwardzuniga <4593@holbertonschool.com>
2022-07-13 17:31:33 -0500

    Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- script that lists the number of records
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
