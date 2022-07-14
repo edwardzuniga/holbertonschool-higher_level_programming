f45722e1320d621a9fb01a08d84c398e9980e92e
edwardzuniga <4593@holbertonschool.com>
2022-07-14 14:43:53 -0500

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- ists all privileges of the MySQL user 1 an user 2
-SHOW GRANTS FOR 'user_0d_1'@'localhost'
-SHOW GRANTS FOR 'user_0d_2'@'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
