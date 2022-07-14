c33977a64b48c49241270ca41023c2ff609dab14
edwardzuniga <4593@holbertonschool.com>
2022-07-14 15:00:15 -0500

    ities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- ists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT id, name FROM cities  WHERE state_id = ORDER BY id;
+SELECT id, name FROM cities WHERE state_id = 1 ORDER BY id;
