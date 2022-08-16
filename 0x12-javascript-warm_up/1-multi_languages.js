22ba6828b131b63118b61fa625bbac6dd866ff46
edwardzuniga <4593@holbertonschool.com>
2022-08-16 09:09:39 -0500

    task 1. 3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-
-const palabra1 = 'C is fun';
-const palabra2 = 'Python is cool';
-const palabra3 = 'Javascript is amazing';
-
-console.log(palabra1);
-console.log(palabra2);
-console.log(palabra3);
+'use strict';
+console.log('C is fun\nPython is cool\nJavascript is amazing');
