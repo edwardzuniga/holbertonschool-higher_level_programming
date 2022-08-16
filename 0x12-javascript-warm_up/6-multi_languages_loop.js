80e0d1d019650f7ecf510d5f9039be6be2a40468
edwardzuniga <4593@holbertonschool.com>
2022-08-16 09:42:01 -0500

    task 6. Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const i of myVar) {
-  console.log(i);
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
 }
