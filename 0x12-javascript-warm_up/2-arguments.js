2faf251a6bc1dd52eb06241e09bf42e0fdeb249b
edwardzuniga <4593@holbertonschool.com>
2022-08-16 09:21:42 -0500

    task 2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-    console.log('No argument');
-  } else if (process.argv.length === 3) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
+const myVar = process.argv.length;
+if (myVar === 2) {
+  console.log('No argument');
+} else if (myVar === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
