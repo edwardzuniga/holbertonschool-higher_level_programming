fd1702454fb1234925e49e4208e9beda2da6a4b2
edwardzuniga <4593@holbertonschool.com>
2022-08-17 14:53:33 -0500

    task 2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-
 module.exports = class Rectangle {
     constructor (w, h) {
-      if ((w = parseInt(w)) > 0 &&
-          (h = parseInt(h)) > 0) {
+      if (w > 0 && h > 0) {
         this.width = w;
         this.height = h;
       }
