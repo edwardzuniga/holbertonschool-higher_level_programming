02a0bf0ef5a35392dde80f0a1c97932f733c4908
edwardzuniga <4593@holbertonschool.com>
2022-08-17 15:52:59 -0500

    task 4. Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -6,18 +6,21 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
+
   rotate () {
-    let tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
+    const temporal = this.height;
+    this.height = this.width;
+    this.width = temporal;
   }
+
   double () {
-    this.width *= 2;
     this.height *= 2;
+    this.width *= 2;
   }
 };
