0bf6951d42a71776893a98460c85779b39becda8
edwardzuniga <4593@holbertonschool.com>
2022-08-17 19:34:24 -0500

    task 6. How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,15 +1,21 @@
 #!/usr/bin/node
-const request = require('request');
 
-request.get(process.argv[2], (error, response, body) => {
-  if (!error) {
-    const completed = {};
-    JSON.parse(body).forEach((tasks) => {
-      if (tasks.completed && completed[tasks.userId] === undefined) {
-        completed[tasks.userId] = 1;
-      } else if (tasks.completed) {
-        completed[tasks.userId] += 1;
-      }
-    });
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
   }
+  const completed = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed === true) {
+      if (completed[task.userId]) {
+        completed[task.userId]++;
+      } else {
+        completed[task.userId] = 1;
+      }
+    }
+  }
+  console.log(completed);
 });
