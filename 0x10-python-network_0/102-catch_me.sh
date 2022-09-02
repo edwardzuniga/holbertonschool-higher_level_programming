8ac46320cb82545087d035f5cf376591f54177d8
edwardzuniga <4593@holbertonschool.com>
2022-09-02 11:52:30 -0500

    task 8. Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that makes a request to 0.0.0.0:5000/catch_me
-curl 0.0.0.0:5000/catch_me -sLX PUT -d "user_id=98" -H "Origin: You got me!"
+curl -sLX PUT -d "user_id=98" -H "You got me!"  0.0.0.0:5000/catch_me
