2faa5d90c2cfaa5263e5951ff3a7de45cf8f9a20
edwardzuniga <4593@holbertonschool.com>
2022-09-02 11:39:12 -0500

    task  5. cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a POST request to the passed URL, and displays the body of the re
-curl -s "$1"-X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+# Script that takes in a URL, sends a POST request to the passed URL
+curl -sX POST "$1" --data "email=test@gmail.com&subject=I will always be here for PLD"
