f943ccaf810ac8ed7a8f986a657f7b77f18b35e0
edwardzuniga <4593@holbertonschool.com>
2022-09-03 11:19:05 -0500

    task 8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This Python script takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        r_dict = response.json()
+        id = r_dict.get('id')
+        name = r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except:
+        print("Not a valid JSON")
